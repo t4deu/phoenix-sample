6bf868d82acc73ef8bdbfd0caa1677cf4c34d278
Tadeu Valentt <tadeu.valentt@gmail.com>
2018-03-21 15:16:36 -0300

    Add category tests

---
+++ test/models/category_repo_test.exs
@@ -0,0 +1,16 @@
+defmodule Showme.CategoryRepoTest do
+  use Showme.ModelCase
+
+  alias Showme.Category
+
+  test "alphabetical/1 orders by name" do
+    Repo.insert!(%Category{name: "c"})
+    Repo.insert!(%Category{name: "a"})
+    Repo.insert!(%Category{name: "b"})
+
+    query = Category |> Category.alphabetical()
+    query = from(c in query, select: c.name)
+
+    assert Repo.all(query) == ~w(a b c)
+  end
+end
